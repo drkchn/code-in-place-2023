1b728de28b16a1d00035eb4b05795911b00c9c35
drkchn <57195399+drkchn@users.noreply.github.com>
2023-05-04 16:34:08 -0700

    Added Spread Beepers Karel solution

---
+++ Spread Beepers Karel.py
@@ -0,0 +1,65 @@
+from karel.stanfordkarel import *
+
+"""
+Each row starts with a stack of beepers. Karel should pick them
+up, one at a time, and spread them down the row. 
+Caution! Karel can't count, and starts with infinite beepers infinite
+her bag. How can you solve this puzzle?
+
+Spread Beepers Karel Section problem
+Code in Place 2023
+May 4th, 2023
+"""
+
+
+def main():
+
+    # We first move Karel forward to initialize our algorithm
+    move()
+
+    # We continue running Karel when there's beepers in the second column from the left
+    while beepers_present():
+        pick_beeper()
+        if no_beepers_present():
+            # If there's no beepers, then this is the last beeper in the second column, and we want to move Karel back to the ending position
+            last_beeper()
+
+        # We keep moving Karel forward when there are no beepers present in the row. This means that we are not at the end of the row yet!
+        while beepers_present():
+            move()
+
+        # We check if Karel faces east, because if Karel faces east and stops moving, then we know for a fact we are at the end of the row!
+        if facing_east():
+            beeper_end_of_row()
+
+    # Once the while loop is finished running, Karel is facing the incorrect direction, so we turn her around, and conclude our program
+    turn_around()
+
+
+def reset_Karel():
+    while front_is_clear():
+        move()
+    turn_around()
+
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+
+
+def beeper_end_of_row():
+    put_beeper()
+    turn_around()
+    reset_Karel()
+    move()
+
+
+def last_beeper():
+    put_beeper()
+    turn_around()
+    move()
+
+
+# There is no need to edit code beyond this point
+if __name__ == "__main__":
+    main()
